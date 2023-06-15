96f0825f3ab9886430892c5e00675ff90e8180b4
hambergjesse <hambergjesse@gmail.com>
2023-06-15 12:18:42 +0300

    added more eurojackpot past results and misc improvements

--- src/app.tsx
+++ src/app.tsx
@@ -5,12 +5,14 @@ import {
   euroJackpotData,
 } from "./functions/algorithm";
 
+import { Copyright } from "./components/Copyright";
+
 export const App = () => {
   const [data, setData] = useState(optimalEuroJackpotNumbers);
 
-  const { numbers, euroNumbers } = data;
+  const { numbers, starNumbers } = data;
 
-  const fetchNumber = () => {
+  const fetchNumbers = () => {
     const updatedData = generateOptimalNumbers(euroJackpotData); // Generate new optimal numbers
     setData(updatedData); // Update the state with the new numbers
   };
@@ -19,18 +21,21 @@ export const App = () => {
     <main className="app__wrapper">
       <h1>EuroJackpot Numbers</h1>
       <section className="number__container">
+        <h2>normal numbers</h2>
         <ul className="lottery-numbers">
           {numbers.map((number) => (
             <li key={number}>{number}</li>
           ))}
         </ul>
+        <h2>star numbers</h2>
         <ul className="euro-numbers">
-          {euroNumbers.map((number) => (
+          {starNumbers.map((number) => (
             <li key={number}>{number}</li>
           ))}
         </ul>
       </section>
-      <button onClick={fetchNumber}>Generate numbers</button>
+      <button onClick={fetchNumbers}>Generate</button>
+      <Copyright />
     </main>
   );
 };
---
+++ src/components/Copyright.tsx
@@ -0,0 +1,38 @@
+export const Copyright = () => {
+  const currYear = new Date().getFullYear();
+
+  type Style = {
+    [key: string]: string;
+  };
+
+  const sectStyle: Style = {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    fontFamily: "Arial",
+    alignItems: "center",
+    position: "absolute",
+    bottom: "50px",
+    gap: "6px",
+  };
+
+  const aStyle: Style = {
+    fontSize: "16px",
+    lineHeight: "16px",
+    color: "#ecedf3",
+  };
+
+  const pStyle: Style = {
+    fontSize: "14px",
+    color: "#96979b",
+  };
+
+  return (
+    <section className="copyright__container" style={sectStyle}>
+      <a href="mailto:contact@hambergjesse.com" style={aStyle}>
+        contact@hambergjesse.com
+      </a>
+      <p style={pStyle}>© {currYear} Jesse Hamberg. All rights reserved.</p>
+    </section>
+  );
+};
